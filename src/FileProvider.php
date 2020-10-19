a6bf54866085cf6e18213f0897f44e21e8cc4d3d
Václav Pelíšek <peldax@gmail.com>
2020-10-19 23:12:11 +0200

    Create FileProvider.php

---
+++ src/FileProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Graphpinator\Nette;
+
+final class FileProvider implements \Graphpinator\Module\Upload\FileProvider
+{
+    use \Nette\SmartObject;
+
+    private \Nette\Http\Request $request;
+
+    public function __construct(\Nette\Http\Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function getMap() : \Graphpinator\Json
+    {
+        return $this->request->getPost('map');
+    }
+
+    public function getFile(string $key) : \Psr\Http\Message\UploadedFileInterface
+    {
+        $file = $this->request->getFile($key);
+
+        return new \GuzzleHttp\Psr7\UploadedFile(
+            $file->getTemporaryFile(),
+            $file->getSize(),
+            $file->getError(),
+            $file->getName(),
+            $_FILES[$key]['type'],
+        );
+    }
+}
