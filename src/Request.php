6651c4d3b51cf8b378d24999101f6bcff3b26afd
Václav Pelíšek <peldax@gmail.com>
2020-10-19 17:03:22 +0200

    Create Request.php

---
+++ src/Request.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Graphpinator\Nette;
+
+final class Request extends \Graphpinator\Request
+{
+    public static function fromNetteHttpRequest(\Nette\Http\Request $request) : self
+    {
+        $method = $request->getMethod();
+
+        if (!\in_array($method, ['GET', 'POST'], true)) {
+            throw new \Graphpinator\Exception\Request\InvalidMethod();
+        }
+
+        $contentType = $request->getHeader('Content-Type');
+
+        if (\is_string($contentType) && \str_starts_with($contentType, 'multipart/form-data')) {
+            if ($method === 'POST' && \array_key_exists('operations', $request->getPost())) {
+                return self::fromJson(\Graphpinator\Json::fromString($request->getPost('operations'));
+            }
+
+            throw new \Graphpinator\Exception\Request\InvalidMultipartRequest();
+        }
+
+        switch ($contentType) {
+            case 'application/graphql':
+                return new self($request->getRawBody());
+            case 'application/json':
+                return self::fromJson(\Graphpinator\Json::fromString($request->getRawBody()));
+            default:
+                return self::fromJson(\Graphpinator\Json::fromObject((object) $request->getQuery()));
+        }
+    }
+}
