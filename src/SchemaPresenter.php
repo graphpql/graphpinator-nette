e0cd0ea0b458a18d952838c5dc0c125af6233c48
Vossik <vosik.m@seznam.cz>
2021-04-28 18:20:18 +0200

    Applied HTML Visitor

--- src/SchemaPresenter.php
+++ src/SchemaPresenter.php
@@ -15,9 +15,17 @@ final class SchemaPresenter extends \Nette\Application\UI\Presenter
 
     public function actionDefault() : void
     {
-        $printer = new \Graphpinator\Printer\Printer(sorter: new \Graphpinator\Printer\TypeKindSorter());
+        $printer = new \Graphpinator\Printer\Printer(new \Graphpinator\Printer\HtmlVisitor(), new \Graphpinator\Printer\TypeKindSorter());
 
         $this->template->setFile(__DIR__ . '/schema.latte');
         $this->template->schema = $printer->printSchema($this->schema);
+    }
+
+    public function actionText() : void
+    {
+        $printer = new \Graphpinator\Printer\Printer(new \Graphpinator\Printer\TextVisitor(), new \Graphpinator\Printer\TypeKindSorter());
+        $response = new \Graphpinator\Nette\TextFileResponse($printer->printSchema($this->schema));
+
+        $this->sendResponse($response);
     }
 }
